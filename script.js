26d218b4af1185beace7be91ca79da73d0c762c0
Justin Kellogg <jlkellogg18@outlook.com>
2020-02-13 11:43:49 -0700

    added comments throughout some functionality elements

--- script.js
+++ script.js
@@ -4,29 +4,35 @@ var $currentDay = document.querySelector("#currentDay");
 //Refreshes the time on the scheduler every second
 setTime(); //calls on page load
 var interval = setInterval(setTime, 1000); //refreshes every second
-function setTime(){
-    var date = moment().format("dddd, MMMM Do YYYY, k:mm:ss");
-    $currentDay.innerHTML = date;
+function setTime() {
+  var date = moment().format("dddd, MMMM Do YYYY, k:mm:ss");
+  $currentDay.innerHTML = date;
 }
 //Used to check the hour so the background can be changed
+//for the past, active, and future time blocks.
 $(".hour").each(function() {
-    $(this)
+  $(this)
     .removeClass("active")
     .removeClass("past")
-    .removeClass("future")
-    var currentHour = moment().hours();
-    var elementHour = this.innerHTML;
-    if(currentHour == elementHour){
-        $(this).addClass("present");
-        $(this).siblings().addClass("present");
-    } else if (currentHour > elementHour){
-        $(this).addClass("past");
-        $(this).siblings().addClass("past");
-    } else {
-        $(this).addClass("future");
-        $(this).siblings().addClass("future");
-    }
-    });
-
+    .removeClass("future");
+  var currentHour = moment().hours();
+  var elementHour = this.innerHTML;
+  if (currentHour == elementHour) {
+    $(this).addClass("present");
+    $(this)
+      .siblings() //targets all siblings to change color throughout entire row.
+      .addClass("present");
+  } else if (currentHour > elementHour) {
+    $(this).addClass("past");
+    $(this)
+      .siblings() //targets all siblings to change color throughout entire row.
+      .addClass("past");
+  } else {
+    $(this).addClass("future");
+    $(this)
+      .siblings() //targets all siblings to change color throughout entire row.
+      .addClass("future");
+  }
+});
 
 

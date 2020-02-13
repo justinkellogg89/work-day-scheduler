79e3acffdd03c112caf31bddb54aaade0708eb8c
Justin Kellogg <jlkellogg18@outlook.com>
2020-02-13 09:29:01 -0700

    setup the hour verification

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
-var $bannerDate = document.querySelector("#banner-date");
-var $middleColumn = document.querySelector(".col-6")
+var $currentDay = document.querySelector("#currentDay");
+// var $hour = document.querySelector(".hour");
 
 //Refreshes the time on the scheduler every second
 setTime(); //calls on page load
 var interval = setInterval(setTime, 1000); //refreshes every second
 function setTime(){
-    var date = new Date();
-    $bannerDate.innerHTML = date;
+    var date = moment().format("dddd, MMMM Do YYYY, k:mm:ss");
+    $currentDay.innerHTML = date;
 }
 //Used to check the hour so the background can be changed
-var hourCheck = new Date();
-var hour = hourCheck.getHours();
-
-
-
+$(".hour").each(function() {
+    $(this)
+    .removeClass("active")
+    .removeClass("past")
+    .removeClass("future")
+    var currentHour = moment().hours();
+    var elementHour = this.innerHTML;
+    console.log(this);
+    if(currentHour == elementHour){
+        $(this).addClass("present");
+        $(this).siblings().addClass("present");
+    } else if (currentHour > elementHour){
+        $(this).addClass("past");
+        $(this).siblings().addClass("past");
+    } else {
+        $(this).addClass("future");
+        $(this).siblings().addClass("future");
+    }
+    });
+console.log(moment().hours());
 
